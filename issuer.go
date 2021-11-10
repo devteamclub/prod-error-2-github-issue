e9555bd1cf28e0faceda0b52e3b4005aed6ac1ae
Igor German <dlyakontakt@gmail.com>
2021-11-10 08:57:45 +0300

    changed error struct, did some refactoring

--- issuer.go
+++ issuer.go
@@ -8,6 +8,8 @@ import (
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 	"os"
+	"regexp"
+	"strconv"
 	"time"
 )
 
@@ -17,81 +19,9 @@ type PubSubMessage struct {
 type ProductionError struct {
 	InsertId    string `json:"insertId"`
 	JsonPayload struct {
-		Error  string `json:"Error"`
-		Locals struct {
-			CurrentBattleId int         `json:"current-battle-id"`
-			CurrentCrew     interface{} `json:"current-crew"`
-			CurrentEvent    struct {
-				Admins          interface{} `json:"admins"`
-				Battles         interface{} `json:"battles"`
-				ContractStatus  string      `json:"contractStatus"`
-				DateEnd         int         `json:"dateEnd"`
-				DateStart       int         `json:"dateStart"`
-				Description     string      `json:"description"`
-				Djs             interface{} `json:"djs"`
-				Feature         bool        `json:"feature"`
-				Hosts           interface{} `json:"hosts"`
-				Id              int         `json:"id"`
-				Image           string      `json:"image"`
-				Judges          interface{} `json:"judges"`
-				LocationAddress string      `json:"locationAddress"`
-				LocationLat     int         `json:"locationLat"`
-				LocationLng     int         `json:"locationLng"`
-				OrgId           int         `json:"orgId"`
-				Organizer       struct {
-					Avatar     string `json:"avatar"`
-					DancerName string `json:"dancerName"`
-					Id         int    `json:"id"`
-				} `json:"organizer"`
-				PaymentMethod struct {
-				} `json:"paymentMethod"`
-				Period string `json:"period"`
-				Status string `json:"status"`
-				Tiers  []struct {
-					Cost        int    `json:"cost"`
-					Description string `json:"description"`
-					Id          int    `json:"id"`
-					Title       string `json:"title"`
-				} `json:"tiers"`
-				Title   string `json:"title"`
-				Website string `json:"website"`
-			} `json:"current-event"`
-			CurrentEventId     int         `json:"current-event-id"`
-			CurrentOrg         interface{} `json:"current-org"`
-			CurrentPermissions struct {
-				AgreementSigned           int  `json:"agreementSigned"`
-				IsPayCheckboxChecked      bool `json:"isPayCheckboxChecked"`
-				IsShowCheckInButton       bool `json:"isShowCheckInButton"`
-				IsShowCheckInOnDayButton  bool `json:"isShowCheckInOnDayButton"`
-				IsShowCheckInPeopleButton bool `json:"isShowCheckInPeopleButton"`
-				IsShowEditButton          bool `json:"isShowEditButton"`
-				IsShowPayCheckbox         bool `json:"isShowPayCheckbox"`
-				IsShowPurchaseButton      bool `json:"isShowPurchaseButton"`
-				IsShowViewTicketButton    bool `json:"isShowViewTicketButton"`
-				IsUserAdmin               bool `json:"isUserAdmin"`
-				IsUserCheckedIn           bool `json:"isUserCheckedIn"`
-				IsUserOrgAdmin            bool `json:"isUserOrgAdmin"`
-				WaiverSigned              int  `json:"waiverSigned"`
-			} `json:"current-permissions"`
-			CurrentUser struct {
-				Active     bool   `json:"active"`
-				Avatar     string `json:"avatar"`
-				Birthday   string `json:"birthday"`
-				Card       string `json:"card"`
-				City       string `json:"city"`
-				Country    string `json:"country"`
-				DancerName string `json:"dancerName"`
-				Email      string `json:"email"`
-				FullName   string `json:"fullName"`
-				Id         int    `json:"id"`
-				Provider   string `json:"provider"`
-				Socials    struct {
-				} `json:"socials"`
-				State string `json:"state"`
-				Uid   string `json:"uid"`
-			} `json:"current-user"`
-		} `json:"Locals"`
-		Stack string `json:"Stack"`
+		Error  string                 `json:"Error"`
+		Locals map[string]interface{} `json:"Locals"`
+		Stack  string                 `json:"Stack"`
 	} `json:"jsonPayload"`
 	LogName          string    `json:"logName"`
 	ReceiveTimestamp time.Time `json:"receiveTimestamp"`
@@ -110,44 +40,116 @@ type ProductionError struct {
 }
 
 func init() {
-	_, isFound := os.LookupEnv("github_token")
+	_, isFound := os.LookupEnv("GITHUB_TOKEN")
 	if isFound == false {
 		panic(errors.New("couldn't find a token"))
 	}
-	_, isFound = os.LookupEnv("github_user")
+	_, isFound = os.LookupEnv("GITHUB_USER")
 	if isFound == false {
 		panic(errors.New("couldn't find a username"))
 	}
-	_, isFound = os.LookupEnv("github_repo")
+	_, isFound = os.LookupEnv("GITHUB_REPO")
 	if isFound == false {
 		panic(errors.New("couldn't find a repository name"))
 	}
 }
 
 func CreateGithubIssue(ctx context.Context, m PubSubMessage) error {
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("github_token")},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-	githubClient := github.NewClient(tc)
+	client := initGithubClient(ctx)
+	err, issue := parseErrorMessage(m)
 
-	var issueTitle string
-	var issueBody string
-	var newError ProductionError
-	err := json.Unmarshal(m.Data, &newError)
-	if err != nil {
-		issueTitle = "Production error"
-		issueBody = string(m.Data)
-	} else {
-		issueTitle = fmt.Sprintf("Prod: %s", newError.JsonPayload.Error)
-		issueBody = fmt.Sprintf("Stack:\n%s\nLocals:\n%s", newError.JsonPayload.Stack, newError.JsonPayload.Locals)
+	var existingIssue *github.Issue
+	if err == nil {
+		existingIssue = checkIssueExists(ctx, *client, *issue.Title)
 	}
 
-	newIssue := github.IssueRequest{Title: &issueTitle, Body: &issueBody}
-	_, _, err = githubClient.Issues.Create(ctx, os.Getenv("github_user"), os.Getenv("github_repo"), &newIssue)
+	if existingIssue != nil {
+		err := updateIssue(ctx, *client, existingIssue)
+		if err != nil {
+			panic(err)
+		}
+		return err
+	}
 
+	err = publishIssue(ctx, *client, issue)
 	if err != nil {
 		panic(err)
 	}
+	return nil
+}
+
+func initGithubClient(ctx context.Context) *github.Client {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	githubClient := github.NewClient(tc)
+	return githubClient
+}
+
+func parseErrorMessage(m PubSubMessage) (error, *github.Issue) {
+	var issueTitle string
+	var issueBody string
+	var newError ProductionError
+	var issue github.Issue
+	err := json.Unmarshal(m.Data, &newError)
+	if err == nil {
+		issueTitle = fmt.Sprintf("Prod err: %s", newError.JsonPayload.Error)
+		issueBody = fmt.Sprintf("## Stack:\n```%s```\n## Locals:\n```%s```", newError.JsonPayload.Stack, newError.JsonPayload.Locals)
+	} else {
+		issueTitle = "Production error"
+		issueBody = string(m.Data)
+	}
+	issue.Title = &issueTitle
+	issue.Body = &issueBody
+	return err, &issue
+}
+
+func checkIssueExists(ctx context.Context, client github.Client, issueTitle string) *github.Issue {
+	issues, _, err := client.Issues.ListByRepo(ctx, os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_REPO"), nil)
+	if err != nil {
+		return nil
+	}
+
+	re, _ := regexp.Compile(fmt.Sprintf(`%s \(\d*\)`, issueTitle))
+	for _, issue := range issues {
+		if re.MatchString(*issue.Title) {
+			return issue
+		}
+	}
+	return nil
+}
+
+func updateIssue(ctx context.Context, client github.Client, issue *github.Issue) error {
+	newTitle, err := incrementCounter(*issue.Title)
+	if err != nil {
+		return err
+	}
+
+	_, _, err = client.Issues.Edit(ctx, os.Getenv("GITHUB_USER"),
+		os.Getenv("GITHUB_REPO"), *issue.Number, &github.IssueRequest{Title: &newTitle, Body: issue.Body})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func publishIssue(ctx context.Context, client github.Client, issue *github.Issue) error {
+	// add error counter to the title
+	*issue.Title = *issue.Title + ` (1)`
+	newIssue := github.IssueRequest{Title: issue.Title, Body: issue.Body}
+	_, _, err := client.Issues.Create(ctx, os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_REPO"), &newIssue)
 	return err
+}
+
+func incrementCounter(t string) (string, error) {
+	re, _ := regexp.Compile(`\(\d*\)$`)
+	res := re.FindStringSubmatchIndex(t)
+	counter, err := strconv.Atoi(t[res[0]+1 : res[1]-1])
+	if err != nil {
+		return "", err
+	}
+	counter = counter + 1
+	newTitle := t[:res[0]] + "(" + strconv.Itoa(counter) + ")"
+	return newTitle, nil
 }
